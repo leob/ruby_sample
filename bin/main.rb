668c1c66c8a1215e9341ceb4b4682244e6c909ea
leob <leob@users.noreply.github.com>
2014-08-31 16:41:20 +0200

    main file: some Ruby best practices for the startup script

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require_relative '../lib/desktop_dir_indexer'
 
 def display(dir_tree)
@@ -12,7 +14,12 @@ def display(dir_tree)
    end
 end
 
-dir = ARGV[0]
+begin
+    dir = ARGV[0]
 
-dir_tree = DesktopDirIndexer.index dir, false
-display(dir_tree) if dir_tree
+    dir_tree = DesktopDirIndexer.index dir, false
+    display(dir_tree) if dir_tree
+rescue => e
+    $stderr.puts e.message
+    exit 1
+end
